df4f144d33e371ddbbb259352da391afbbd804b3
Yery Pedraza <yery.pedraza523@gmail.com>
2022-06-15 15:22:08 -0500

    Create testing script

---
+++ testScript.py
@@ -0,0 +1,49 @@
+# A non-linear operation used to encode and decode luminance or tristimulus values in video or still image systems
+def adjust_gamma(image, gamma=1.0):
+    invGamma = 1.0 / gamma
+    table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)])
+    return cv2.LUT(image.astype(np.uint8), table.astype(np.uint8))
+
+#Upload the model
+model = load_model('faceMask.h5')
+
+#Select test images
+test_images = ['1114.png','1504.jpg', '0072.jpg','0012.jpg','0353.jpg','1374.jpg']
+gamma = 2.0
+fig = plt.figure(figsize = (14,14))
+rows = 3
+cols = 2
+axes = []
+assign = {'0':'Mask','1':"No Mask"}
+for j,im in enumerate(test_images):
+    image =  cv2.imread(os.path.join(image_directory,im),1)
+    image =  adjust_gamma(image, gamma=gamma)
+    (h, w) = image.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300,300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    #Use of ImageNet architecture for verify
+    cvNet.setInput(blob)
+    detections = cvNet.forward()
+    print(detections.shape)
+    for i in range(0, detections.shape[2]):
+        try:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            frame = image[startY:endY, startX:endX]
+            confidence = detections[0, 0, i, 2]
+            #print(confidence)
+            if confidence > 0.2:
+                im = cv2.resize(frame,(img_size,img_size))
+                im = np.array(im)/255.0
+                im = im.reshape(1,124,124,3)
+                result = model.predict(im)
+                if result>0.5:
+                    label_Y = 1
+                else:
+                    label_Y = 0
+                cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+                cv2.putText(image,assign[str(label_Y)] , (startX, startY-10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (36,255,12), 2)
+        
+        except:pass
+    axes.append(fig.add_subplot(rows, cols, j+1))
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.show()
